9accac7de9d7a2287a3571fa881871657dd853e7
Preet Dudhat <preet.dudhat0307@gmail.com>
2026-02-14 15:06:25 +0530

    14/2/206

--- lib/version.ts
+++ lib/version.ts
@@ -1,6 +1,6 @@
 export const latestVersion = {
-    version: "1.5.4+5",
-    build: 5,
+    version: "1.5.4+6",
+    build: 6,
     releaseDate: "2026-02-13",
     downloadUrl: "https://klypt.vercel.app/download",
     changelog: [
